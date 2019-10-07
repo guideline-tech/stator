0287f557836935b2d4208a98818c8293a16f4124
Mike Nelson <mike@guideline.com>
2019-10-07 09:34:53 -0400

    allow explicitly setting state timestamps in creation / updates of records

--- lib/stator/version.rb
+++ lib/stator/version.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 module Stator
+
   MAJOR       = 0
   MINOR       = 3
-  PATCH       = 0
+  PATCH       = 1
   PRERELEASE  = nil
 
-  VERSION = [MAJOR, MINOR, PATCH, PRERELEASE].compact.join('.')
+  VERSION = [MAJOR, MINOR, PATCH, PRERELEASE].compact.join(".")
+
 end
